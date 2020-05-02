0c9521498d4d878a16d600c670bb1516a5fba51e
eastline <eastline@maycan.co.kr>
2020-05-02 09:38:01 +0900

    momentum_clone

---
+++ weather.js
@@ -0,0 +1,53 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "dd85c701537fff7cb8d69b215710e998";
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`).then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    })
+}
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    }; // 객체의 변수의 이름과 객체의 key 이름을 같게 저장할때는 위 같이 쓸수 있음 다를때는 latitude = latitude, longitude = longitude 로 적어야함
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+    console.log("Can't access geo location")
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError)
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
+        askForCoords();
+    } else {
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
